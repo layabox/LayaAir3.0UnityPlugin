59bf341fdf667b0eb15af325a2df778c406b1c99
zringhost11 <38626955@qq.com>
2025-09-24 14:26:36 +0800

    fix

--- Editor/Export/component/ParticleSystemData.cs
+++ Editor/Export/component/ParticleSystemData.cs
@@ -521,7 +521,7 @@ internal class ParticleSystemData
         writeRotationOverLifetime(particleSystem, particleSystemData);
         writeRotationBySpeed(particleSystem, particleSystemData);
         writeExternalForces(particleSystem, particleSystemData, map);
-        writeInheritVelocity(particleSystem, compData);
+        writeInheritVelocity(particleSystem, particleSystemData);
         writeNoise(particleSystem, particleSystemData);
         writeCollision(particleSystem, particleSystemData, map);
         writeSubEmittersModule(particleSystem, particleSystemData, map);
